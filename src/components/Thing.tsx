f0d3dd44e7634169b26e4d876ce88b82bdf48ddd
Carson Powers <carson.powers@gmail.com>
2024-11-29 13:12:12 -0600

    feat: convert Thing component to a client component

--- src/components/Thing.tsx
+++ src/components/Thing.tsx
@@ -1,7 +1,9 @@
+'use client'
+
 import Wheel from '../components/Wheel'
 import UI from '../components/UI'
 
-export default async function Thing({ movies }) {
+export default function Thing({ movies }) {
   return (
     <>
       <UI movies={movies}></UI>
