6829d901c81b2af20ab3a12e0cc7fb613450f5f9
Carson Powers <carson.powers@gmail.com>
2024-11-30 12:20:33 -0600

    feat: extract DownButton component from Wheel for better modularity and maintainability

--- src/components/wheel.tsx
+++ src/components/wheel.tsx
@@ -5,6 +5,7 @@ import { Wheel as wheel } from 'spin-wheel'
 import React, { useEffect, useRef, useState } from 'react'
 import { pipe } from 'lodash/fp'
 import { easeOutElastic } from 'easing-utils'
+import DownButton from './DownButton'
 
 interface WheelItem {
   currentIndex: number
@@ -141,53 +142,6 @@ const getDirection = (currentIndex: number) => {
 
 const destroyWheel = () => {
   document.wheelInstance.remove()
-}
-
-const DownButton = () => {
-  const [isAnimating, setIsAnimating] = useState(false)
-  return (
-    <button
-      className='absolute bottom-2.5 left-1/2 right-1/2 w-24 h-12 transform -translate-x-1/2 z-10 border-2 border-white rounded-lg bg-black'
-      onClick={({ target }) => {
-        const wheelContainer = document.querySelector('#wheel')
-        const wheel = document.querySelector('canvas')
-        const movieListContainer = document.querySelector(
-          '#movie-list-container'
-        )
-
-        if (movieListContainer?.classList.contains('opacity-0')) {
-          movieListContainer?.classList.add('opacity-100')
-          movieListContainer?.classList.remove('opacity-0')
-        } else {
-          movieListContainer?.classList.add('opacity-0')
-          movieListContainer?.classList.remove('opacity-100')
-        }
-
-        if (wheel) {
-          if (isAnimating) {
-            wheelContainer.animate([{ transform: `translate(-50%, -25%)` }], {
-              duration: 1000, // Duration in milliseconds
-              fill: 'forwards', // Maintain the end state after animation
-              easing: 'cubic-bezier(0.075, 0.82, 0.165, 1)',
-            })
-            wheelContainer?.removeAttribute('style')
-            target.innerHTML = '▼'
-          } else {
-            wheelContainer.animate([{ transform: `translate(-50%, 25%)` }], {
-              duration: 1000,
-              fill: 'forwards',
-              easing: 'cubic-bezier(0.075, 0.82, 0.165, 1)',
-            })
-            target.innerHTML = '▲'
-            wheelContainer.style.pointerEvents = 'none'
-          }
-          setIsAnimating(!isAnimating)
-        }
-      }}
-    >
-      ▼
-    </button>
-  )
 }
 
 export default function Wheel({ movies }: { movies: Movie[] }) {
