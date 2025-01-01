2f9ad1b75a72d1635314f6a21e68beac67b2ba0f
Carson Powers <carson.powers@gmail.com>
2024-12-31 18:08:38 -0600

    feat: update Google provider configuration in NextAuth authentication

--- auth.ts
+++ auth.ts
@@ -5,7 +5,9 @@ import Google from "next-auth/providers/google"
 import { cert } from "firebase-admin/app"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  providers: [
+    Google({ clientId: '288775417657-bj6trm0kuqodbqjbu8lmeuvcnchoanpt.apps.googleusercontent.com', clientSecret: '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' }),
+  ],
   adapter: FirestoreAdapter({
     credential: cert({
       projectId: process.env.AUTH_FIREBASE_PROJECT_ID,
