e01c94e6497c0f7f263a501cf06f8f2ff2e4d034
Carson Powers <carson.powers@gmail.com>
2025-12-07 10:03:37 -0600

    feat: add AUTH_URL secret to environment variables in MovieSpinnerStack

--- cdk/lib/movie-spinner-stack.ts
+++ cdk/lib/movie-spinner-stack.ts
@@ -106,6 +106,7 @@ export class MovieSpinnerStack extends cdk.Stack {
       secrets: {
         // All environment variables come from Secrets Manager
         NEXT_PUBLIC_APP_URL: ecs.Secret.fromSecretsManager(appSecret, 'NEXT_PUBLIC_APP_URL'),
+        AUTH_URL: ecs.Secret.fromSecretsManager(appSecret, 'AUTH_URL'),
         NEXT_PUBLIC_FIREBASE_API_KEY: ecs.Secret.fromSecretsManager(appSecret, 'NEXT_PUBLIC_FIREBASE_API_KEY'),
         NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: ecs.Secret.fromSecretsManager(appSecret, 'NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN'),
         NEXT_PUBLIC_FIREBASE_PROJECT_ID: ecs.Secret.fromSecretsManager(appSecret, 'NEXT_PUBLIC_FIREBASE_PROJECT_ID'),
