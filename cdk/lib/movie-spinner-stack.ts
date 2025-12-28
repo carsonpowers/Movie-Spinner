5465dd58e04851d7d7e8f8a1ae310bd906a87174
Carson Powers <carson.powers@gmail.com>
2025-12-27 23:33:50 -0600

    feat: add Firebase environment variables to Dockerfile and AWS deployment workflow

--- cdk/lib/movie-spinner-stack.ts
+++ cdk/lib/movie-spinner-stack.ts
@@ -98,6 +98,15 @@ export class MovieSpinnerStack extends cdk.Stack {
     const container = taskDefinition.addContainer('MovieSpinnerContainer', {
       image: ecs.ContainerImage.fromAsset('..', {
         file: 'Dockerfile',
+        buildArgs: {
+          NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || '',
+          NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || '',
+          NEXT_PUBLIC_FIREBASE_PROJECT_ID: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || '',
+          NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || '',
+          NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || '',
+          NEXT_PUBLIC_FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || '',
+          NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || '',
+        },
       }),
       logging: ecs.LogDrivers.awsLogs({
         streamPrefix: 'movie-spinner',
